c0734fa2501fd5d792e9ff027a5a00cb8f042ebb
maritrotz <86530743+maritrotz@users.noreply.github.com>
2026-09-08 17:47:45 -0400

    Add: rotting-fruit - submission-2

---
+++ Data Structures & Algorithms/rotting-fruit/submission-2.py
@@ -0,0 +1,33 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+
+        def bfs(r,c):
+            directions = [[0,1], [0,-1], [1,0], [-1,0]]
+            for dr, dc in directions:
+                nr, nc = dr + r, dc + c
+                if nr in range(rows) and nc in range(cols) and (nr,nc) not in visited and grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
+                    q.append((nr,nc))
+                    visited.add((nr,nc))
+                    self.fresh -= 1
+            
+        q = deque()
+        visited = set()
+        rows,cols = len(grid), len(grid[0])
+        minutes = 0
+        self.fresh = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 2:
+                    q.append((r,c))
+                    visited.add((r,c))
+                elif grid[r][c] == 1:
+                    self.fresh += 1
+
+        while q and self.fresh > 0:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                bfs(r,c)
+            minutes += 1
+        return minutes if self.fresh == 0 else -1
